0248d90275dc6206bf164a2c6a0f12fcccb34554
carolahn <ahncarolina@gmail.com>
2024-02-27 18:28:40 -0300

    fix: createRoot

--- website/src/index.js
+++ website/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
+const root = document.getElementById('root');
+
+const rootElement = createRoot(root);
+
+rootElement.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
 );
